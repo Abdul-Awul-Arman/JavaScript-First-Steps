06595d2dc99088c1151c2c9bc3215ca80af1ec33
arman52114 <129601164+arman52114@users.noreply.github.com>
2024-04-28 19:38:59 +0600

    add quiz

--- array.js
+++ array.js
@@ -11,3 +11,18 @@
     // console.log(array);
 
     // console.log(data);
+
+    // let array=["lions", "tigers", "bears oh my!"];
+    // let result = array.join(" % ");
+    // console.log(result);
+
+    let array=["arman","sss"];
+    console.log("array");
+
+    let x=["a","b",'c']
+
+    let a=["sss","ddd"]
+
+    let result=array.concat(["aar",a])
+
+    console.log(result);
